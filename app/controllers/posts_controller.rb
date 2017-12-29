3034d1391b0b0c2cfa9096c0b9aedb2a48047e64
Richard Travis <r.z.travis@gmail.com>
2017-12-29 13:50:28 -0700

    Assignment 18 complete

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,12 @@
 class PostsController < ApplicationController
   def index
+    p Post.find(1)
+    p Post.all.count
+    for i in 1..(Post.all.count)
+      if (i - 1) % 5 == 0
+        Post.update(i, title: "SPAM")
+      end
+    end
     @posts = Post.all
   end
 
