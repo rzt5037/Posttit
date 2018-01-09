fb4d6ae096348da3c18674c3a7adebaee1cbeb01
Richard Travis <r.z.travis@gmail.com>
2018-01-08 17:36:52 -0700

    Checkpoint 25 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,9 +34,15 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'r.z.travis@gmail.com',
+admin = User.create!(
+  name:     'Admin User',
+  email:    'admin@example.com',
+  password: 'helloworld',
+  role:     'admin'
+)
+member = User.create!(
+  name:     'Member User',
+  email:    'member@example.com',
   password: 'helloworld'
 )
 
